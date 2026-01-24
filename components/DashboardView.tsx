beb57d640fd8b255323651b3f79e74fcaba97e40
Piramalinfo012 <info@piramalpetroleum.com>
2026-01-24 17:41:41 +0530

    total call

--- components/DashboardView.tsx
+++ components/DashboardView.tsx
@@ -119,7 +119,7 @@ const DashboardView: React.FC<DashboardViewProps> = ({ clients, logs }) => {
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 <DashboardCard
-                    title="Month Activity"
+                    title="Total Call"
                     value={stats.totalCalls}
                     icon="fa-list-check"
                     color="bg-indigo-500"
