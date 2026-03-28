cb11f2b48d91a69d00dbf87d945ea32ad1697524
Piramalinfo012 <info@piramalpetroleum.com>
2026-03-28 17:13:57 +0530

    Fix typo in option value for call disconnected

--- components/FollowUpModal.tsx
+++ components/FollowUpModal.tsx
@@ -142,7 +142,7 @@ setIsSaving(true);
                     <option value="RATE ISSUE">RATE ISSUE</option>
                     <option value="RATE SHARED">RATE SHARED</option>
                     <option value="CALL NOT RECIEVED">CALL NOT RECIEVED</option>
-                    <option value="CALL DISCONNECT">CALL DISCONNECT</option>
+                    <option value="CALL DISCONNECTED">CALL DISCONNECTED</option>
                     <option value="HE WILL ORDER WHEN REQUIRMENT">HE WILL ORDER WHEN REQUIRMENT</option>
                     <option value="PAYMENT PENDING">PAYMENT PENDING</option>
                   </select>
